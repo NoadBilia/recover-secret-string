a1fab58124aa1fa390338456ba59ac8e99bfabaf
noadbilia <noad14@gmail.com>
2021-10-24 16:43:44 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/* Recover secret string */
+// Noad Bilia
+
+function recoverSecret(triplets) {
+      script = [];
+}
+
+
+secret1 = "whatisup";
+triplets = [['t', 'u', 'p'],['w', 'h', 'i'],['t', 's', 'u'],['a', 't', 's'],['h', 'a', 'p'],['t', 'i', 's'],['w', 'h', 's']];
+
+recoverSecret(triplets);
+
+export {};
