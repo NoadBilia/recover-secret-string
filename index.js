729675950ba6a3c53651531413559af92b04ab68
noadbilia <noad14@gmail.com>
2021-10-27 16:25:43 +0300

    Project completion

--- index.js
+++ index.js
@@ -2,51 +2,65 @@
 // Noad Bilia
 
 // func Recover Secret
-function recoverSecret(triplets) {
-      let script = []; // Array Recover Secret
-      for (let indexOfArrayNum in triplets) {
-            let firstTav = triplets[indexOfArrayNum][0];
-            let secondTav = triplets[indexOfArrayNum][1];
-            let thirdTav = triplets[indexOfArrayNum][2];
-
-            // If the first character does not exist in the array
-            if (script.indexOf(firstTav) === -1)       
-                  script.unshift(firstTav);
-                  
-            // If the second character does not exist in the array
-            if (script.indexOf(secondTav) === -1) {      
-                  script.splice(script.indexOf(firstTav)+1, 0,secondTav);
+const recoverSecret = (array) => {
+      let indexOfArray = 0;
+      let flagWeFound = false;
+      let scriptSrt = "";
+      let twoDimensionalArray = array;
+      while (emptyArray(twoDimensionalArray)) {
+            for (let valueOfArrayNum of twoDimensionalArray[indexOfArray]) {
+                  if (searchTav(valueOfArrayNum, twoDimensionalArray)) {
+                        scriptSrt += valueOfArrayNum;
+                        twoDimensionalArray = deleteTav(valueOfArrayNum, twoDimensionalArray)
+                        flagWeFound = true
+                        break;
+                  }
             }
-            // If there is a Y in the array, and also that Y is less than X 
-            if (script.indexOf(secondTav) !== -1 && script.indexOf(secondTav) < script.indexOf(firstTav)) { 
-                  script.splice(script.indexOf(secondTav), 1);
-                  let tav = locationSearch(triplets[indexOfArrayNum],secondTav)
-                  script.splice((script.indexOf(tav)+1), 0,secondTav);
+            if (flagWeFound) {
+                indexOfArray = 0;
+                flagWeFound = false; 
+            } else {
+                  indexOfArray++
             }
-            // If the third character does not exist in the array
-            if (script.indexOf(thirdTav) === -1)            
-                  script.splice(script.indexOf(secondTav)+1, 0,thirdTav);
-                  
-            // If there is a Z in the array, and also that Z is less than Y  
-            if (script.indexOf(thirdTav) !== -1 && script.indexOf(thirdTav) < script.indexOf(secondTav)) {
-                  script.splice(script.indexOf(secondTav), 1)
-                  let tav = locationSearch(triplets[indexOfArrayNum], thirdTav)
-                  script.splice((script.indexOf(tav)+1), 0,thirdTav);
-            }
-      }      
-      //console.log("Line:39 - whatisup = ", script.join(""));
-      return script.join("");
+      }  
+      return scriptSrt;
 }
 
-// func Location Search Tav
-function locationSearch(arr,indexOfTavInArray) {
-      let num = arr.indexOf(indexOfTavInArray);
-      return (arr[num-1]); // Return Tav
+// func delete Character
+const emptyArray = (twoDimensionalArray) => {
+      let flagEmptyArray = false; 
+      twoDimensionalArray.forEach(arr => {
+            if (arr.length != 0) {
+                  flagEmptyArray = true;
+            }
+      });
+      return flagEmptyArray;
 }
 
-let answerRecoverSecret = "whatisup";
-let triplets = [['t', 'u', 'p'],['w', 'h', 'i'],['t', 's', 'u'],['a', 't', 's'],['h', 'a', 'p'],['t', 'i', 's'],['w', 'h', 's']];
+// func delete Character
+const deleteCharacter = (character, twoDimensionalArray) => {
+      let array = []
+      twoDimensionalArray.forEach(arr => {
+            arr = arr.filter(val => {
+                  return val != character
+            })
+            array.push(arr)
+      });
+      return array // Return Array
+}
 
-recoverSecret(triplets);
+// func search Character
+const searchCharacter = (character, twoDimensionalArray) => {
+      for (let indexOfArray in twoDimensionalArray) {
+            for (let indexOfArrayNum = 1; indexOfArrayNum < twoDimensionalArray[indexOfArray].length; indexOfArrayNum++) { // מנפה בתוך המערך
+                  if (character == twoDimensionalArray[indexOfArray][indexOfArrayNum]) {
+                        return false;
+                  }
+            }  
+      }
+      return true;
+}
 
-export {recoverSecret, locationSearch};
+recoverSecret(array);
+
+export {recoverSecret, deleteCharacter, searchCharacter, emptyArray};
--- index.test.js
+++ index.test.js
@@ -1,13 +1,62 @@
-import {recoverSecret, locationSearch} from './index.js';
+import {recoverSecret, deleteCharacter, searchCharacter, emptyArray} from './index.js';
 
 // Main func - Testing Recover Secret
 test('Testing Recover Secret', () => {
-  let input = [['t', 'u', 'p'],['w', 'h', 'i'],['t', 's', 'u'],['a', 't', 's'],['h', 'a', 'p'],['t', 'i', 's'],['w', 'h', 's']];
-  expect(recoverSecret(input)).toBe('whatisup');
+  let arr = [['t', 'u', 'p'],['w', 'h', 'i'],['t', 's', 'u'],['a', 't', 's'],['h', 'a', 'p'],['t', 'i', 's'],['w', 'h', 's']];
+  expect(recoverSecret(arr)).toBe('whatisup');
 });
 
-// func - Testing Location Search
-test('Testing Location Search', () => {
-  let input = ['h', 'a', 'p']
-  expect(locationSearch(input, 'a')).toBe('h');
+// func - Search Character
+test('func - Search', () => {
+  let arr = [['t', 'u', 'p'],['w', 'h', 'i'],['t', 's', 'u'],['a', 't', 's'],['h', 'a', 'p'],['t', 'i', 's'],['w', 'h', 's']];
+  expect(searchCharacter('w', arr)).toBe(true);
+});
+
+// func - Empty Array 1
+test('func - emptyArray 1', () => {
+  let arr = [['t', 'u', 'p'],['w', 'h', 'i'],['t', 's', 'u'],['a', 't', 's'],['h', 'a', 'p'],['t', 'i', 's'],['w', 'h', 's']];
+  expect(emptyArray(arr)).toBe(true);
+});
+
+// func - Empty Array 2
+test('func - emptyArray 2', () => {
+  let arr = [[],[],[],[],[],[],[]];
+  expect(emptyArray(arr)).toBe(false);
+});
+
+
+// func - Delete Character
+test('func - Delete 1', () => {
+  let arr = [['t', 'u', 'p'],['w', 'h', 'i'],['t', 's', 'u'],['a', 't', 's'],['h', 'a', 'p'],['t', 'i', 's'],['w', 'h', 's']];
+  expect(deleteCharacter('w', arr)).toStrictEqual([
+    [ 't', 'u', 'p' ],
+    [ 'h', 'i' ],
+    [ 't', 's', 'u' ],
+    [ 'a', 't', 's' ],
+    [ 'h', 'a', 'p' ],
+    [ 't', 'i', 's' ],
+    [ 'h', 's' ]
+  ]);
+});
+
+// func - Delete Character
+test('func - Delete 2', () => {
+  let arr = [
+    [ 't', 'u', 'p' ],
+    [ 'h', 'i' ],
+    [ 't', 's', 'u' ],
+    [ 'a', 't', 's' ],
+    [ 'h', 'a', 'p' ],
+    [ 't', 'i', 's' ],
+    [ 'h', 's' ]
+  ];
+  expect(deleteCharacter('h', arr)).toStrictEqual([
+    [ 't', 'u', 'p' ],
+    [ 'i' ],
+    [ 't', 's', 'u' ],
+    [ 'a', 't', 's' ],
+    [ 'a', 'p' ],
+    [ 't', 'i', 's' ],
+    [ 's' ]
+  ]);
 });
